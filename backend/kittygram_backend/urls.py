de6c2093f5a8c5e16ef15ebfcfefe24f68ce7525
Eduard-Bodreev <edik-bodreev@mail.ru>
2025-02-10 03:09:33 +0300

    tests3

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,23 +1,25 @@
 from rest_framework import routers
-
 from django.contrib import admin
 from django.urls import include, path
-
 from django.conf import settings
 from django.conf.urls.static import static
-
 from cats.views import AchievementViewSet, CatViewSet
-
+from django.http import HttpResponse
 
 router = routers.DefaultRouter()
 router.register(r"cats", CatViewSet)
 router.register(r"achievements", AchievementViewSet)
 
+# ✅ Функция для корневого пути "/"
+def index(request):
+    return HttpResponse("Kittygram is running!")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
-    path("api/", include("djoser.urls")),  # Работа с пользователями
-    path("api/", include("djoser.urls.authtoken")),  # Работа с токенами
+    path("api/", include("djoser.urls")),
+    path("api/", include("djoser.urls.authtoken")),
+    path("", index),
 ]
 
 if settings.DEBUG:
